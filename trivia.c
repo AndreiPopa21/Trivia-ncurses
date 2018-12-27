03e960f019612345c0580c0ec27c64a9033c5b54
AndreiPopa21 <andrei.popa2199@gmail.com>
2018-12-27 13:00:14 +0200

    Integrate with utils.h

--- trivia.c
+++ trivia.c
@@ -3,26 +3,9 @@
 #include <string.h>
 #include <ncurses.h>
 #include "manager.h"
+#include "utils.h"
 
-#define BUFMAX 300
-#define ANSWER_BUFF 30
-#define QUESTION_BUFF 300
-#define RIGHT_ANSWER_BUFF 2
 #define INITIAL_QUESTIONS_CONTAINER_SIZE 4
-
-typedef struct Question{
-    char* question;
-    char* a_answer;
-    char* b_answer;
-    char* c_answer;
-    char* d_answer;
-    char* right_answer;
-} Question;
-
-
-Question* get_questions(FILE* src,int* questions_count,Question* questions,int* curr_container_size);
-void free_questions_memory(Question* questions, int quest_count);
-Question* resize_questions_container(Question* questions, int* curr_container_size);
 
 int main(int argc, char* argv[]){
 
@@ -73,82 +56,6 @@ int main(int argc, char* argv[]){
     getch();
     endwin();
     return 0;
-}
-
-Question* get_questions(FILE* src,int* questions_count,Question* questions,int *curr_container_size){
-   
-    char buff_question[BUFMAX];
-    char buff_answers[BUFMAX];
-
-    int quest_index = *questions_count;
-    while(fgets(buff_question,BUFMAX,src)!=NULL){
-
-
-            if(quest_index>=(*curr_container_size)){
-                questions= resize_questions_container(questions,curr_container_size);
-                printf("S-a marit containerul\n");
-            }
-            printf("The curr size: %d | The occupied size: %d \n",(*curr_container_size),quest_index+1);
-
-            char* token=strtok(buff_question, "|\n");
-           
-            questions[quest_index].question=(char*)malloc(QUESTION_BUFF*sizeof(char));
-            strcpy(questions[quest_index].question,token);
-            
-            token=strtok(NULL,"|\n");
-            
-          
-            questions[quest_index].right_answer=(char*) malloc(RIGHT_ANSWER_BUFF*sizeof(char));
-            strcpy(questions[quest_index].right_answer,token);
-
-            fgets(buff_answers,BUFMAX,src);
-            
-            token=strtok(buff_answers, "|\n");
-            questions[quest_index].a_answer=(char*)malloc(ANSWER_BUFF*sizeof(char));
-            questions[quest_index].b_answer=(char*)malloc(ANSWER_BUFF*sizeof(char));
-            questions[quest_index].c_answer=(char*)malloc(ANSWER_BUFF*sizeof(char));
-            questions[quest_index].d_answer=(char*)malloc(ANSWER_BUFF*sizeof(char));
-            strcpy(questions[quest_index].a_answer,token);
-            for(int i=0;i<3;i++){
-                token=strtok(NULL,"|\n");
-                switch(i){
-                    case 0:
-                        strcpy(questions[quest_index].b_answer,token);
-                        break;
-                    case 1:
-                        strcpy(questions[quest_index].c_answer,token);
-                        break;
-                    case 2:
-                         strcpy(questions[quest_index].d_answer,token);
-                        break;
-                }
-            }
-        
-            quest_index++;
-    }
-
-    fclose(src);
-    *questions_count=quest_index;
-    return questions;
-}
-
-void free_questions_memory(Question* questions, int quest_count){
-    for(int i=0;i<quest_count;i++){
-        free(questions[i].question);
-        free(questions[i].right_answer);
-        free(questions[i].a_answer);
-        free(questions[i].b_answer);
-        free(questions[i].c_answer);
-        free(questions[i].d_answer);
-    }
-    free(questions);
-}
-
-Question* resize_questions_container(Question* questions, int* curr_container_size){
-    Question *addr;
-    addr= (Question*)realloc(questions,(*curr_container_size)*2*sizeof(Question));
-    *curr_container_size = (*curr_container_size)*2;
-    return addr;
 }
 
 
