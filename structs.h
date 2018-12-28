e057e5c63db53e10811517598a96bed8b4ef9af0
AndreiPopa21 <andrei.popa2199@gmail.com>
2018-12-28 14:01:20 +0200

    Add GameStat struct for future use in storing the game stats at a moment

--- structs.h
+++ structs.h
@@ -10,4 +10,13 @@ typedef struct Question{
     char* right_answer;
 } Question;
 
+typedef struct GameStat{
+    char* curr_question;
+    int curr_question_index;
+    int wrong_answers;
+    int right_answers;
+    int didFifty; //tells the program whether the fifty-fifty option has been used
+    int didSkip; //tells the program whether the skip option has been used
+}GameStat;
+
 #endif
